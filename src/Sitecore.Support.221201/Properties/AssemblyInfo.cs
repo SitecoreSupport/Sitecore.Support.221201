661a23faa586c12059cd7b6fe226e6b638a84487
DK\ALTU <altu@sitecore.net>
2018-07-21 17:19:58 +0300

    Original JqueryModalDialogs file

---
+++ src/Sitecore.Support.221201/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.221201")]
+[assembly: AssemblyProduct("Sitecore.Support.221201")]
+[assembly: ComVisible(false)]
